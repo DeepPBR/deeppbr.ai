767b924582e59d130ce47e276a58dc97540c504f
Miles Thompson <utunga@gmail.com>
2018-09-08 14:42:04 +1200

    make sure that images get included in the package by referencing them from here

--- src/index.js
+++ src/index.js
@@ -8,6 +8,55 @@ import "./assets/css/dpr.css";
 
 // images we need to be sure to include in deployed distribution
 import "./assets/img/bg-masthead.jpg";
+import "./assets/img/examples/00_base.jpg";
+import "./assets/img/examples/00_delit.jpg";
+import "./assets/img/examples/00_norms.jpg";
+import "./assets/img/examples/00_rough.jpg";
+import "./assets/img/examples/01_base.jpg";
+import "./assets/img/examples/01_delit.jpg";
+import "./assets/img/examples/01_norms.jpg";
+import "./assets/img/examples/01_rough.jpg";
+import "./assets/img/examples/02_base.jpg";
+import "./assets/img/examples/02_delit.jpg";
+import "./assets/img/examples/02_norms.jpg";
+import "./assets/img/examples/02_rough.jpg";
+import "./assets/img/examples/03_base.jpg";
+import "./assets/img/examples/03_delit.jpg";
+import "./assets/img/examples/03_norms.jpg";
+import "./assets/img/examples/03_rough.jpg";
+import "./assets/img/examples/04_base.jpg";
+import "./assets/img/examples/04_delit.jpg";
+import "./assets/img/examples/04_norms.jpg";
+import "./assets/img/examples/04_rough.jpg";
+import "./assets/img/examples/05_base.jpg";
+import "./assets/img/examples/05_delit.jpg";
+import "./assets/img/examples/05_norms.jpg";
+import "./assets/img/examples/05_rough.jpg";
+import "./assets/img/examples/06_base.jpg";
+import "./assets/img/examples/06_delit.jpg";
+import "./assets/img/examples/06_norms.jpg";
+import "./assets/img/examples/06_rough.jpg";
+import "./assets/img/examples/07_base.jpg";
+import "./assets/img/examples/07_delit.jpg";
+import "./assets/img/examples/07_norms.jpg";
+import "./assets/img/examples/07_rough.jpg";
+import "./assets/img/examples/08_base.jpg";
+import "./assets/img/examples/08_delit.jpg";
+import "./assets/img/examples/08_norms.jpg";
+import "./assets/img/examples/08_rough.jpg";
+
+// we don't need to references these because 
+// they're refernced in the other js - which the 
+// webpack reads - but what the hey force them to 
+// be included anyway
+import "./assets/webgl/geo/plane.obj";
+import "./assets/webgl/geo/planeSubDiv.obj";
+
+
+
+import "./assets/img/bg-masthead.jpg";
+
+
 
 // js - libraries
 import "bootstrap"; //magic! i dunno
