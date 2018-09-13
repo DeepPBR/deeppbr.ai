31e627686cf45f881ebe2f68fc1023992b59459e
luckybulldozer <luckybulldozer@luckybulldozer.com>
2018-09-13 14:21:40 +1200

    Added extra light, zoom out message

--- src/assets/js/webgl_main.js
+++ src/assets/js/webgl_main.js
@@ -13,7 +13,7 @@ var camera, scene, renderer, controls;
 var mesh;
 
 var pointLight;
-var spotLight;
+var directionalLight;
 //var lampBright;
 
 var imageMapCurrent = 0
@@ -124,7 +124,9 @@ function init() {
     container = document.getElementById( "webgl_container" );
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
-    
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap; // default THREE.PCFShadowMap
+
     var canvasWidth = container.offsetWidth;
     var canvasHeight = container.offsetHeight;
     renderer.setSize( canvasWidth, canvasHeight);
@@ -140,9 +142,12 @@ function init() {
     renderer.gammaInput = true;
     renderer.gammaOutput = true;
 
-    renderer.toneMapping = THREE.ReinhardToneMapping;
-    renderer.toneMappingExposure = 3;
-
+    // renderer.toneMapping = THREE.ReinhardToneMapping;
+    // renderer.toneMappingExposure = 3;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.BasicShadowMap;
+    renderer.shadowMapSoft = true;
+    
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 1000 );
@@ -152,9 +157,39 @@ function init() {
 
     //scene.add( new THREE.HemisphereLight( 0x443333, 0x222233, 4 ) );
     pointLight = new THREE.PointLight( 0xffffff, settings.lampBright, 0 );
-    pointLight.position.set(0,0,5);
+    pointLight.position.set(0,0,4);
+    pointLight.castShadow = true;
+    pointLight.shadowCameraVisible = true;
+        //Set up shadow properties for the light
+    // light.shadow.mapSize.width = 512;  // default
+    // light.shadow.mapSize.height = 512; // default
+    // light.shadow.camera.near = 0.1;       // default
+    // light.shadow.camera.far = 50;      // default
 
     scene.add(pointLight);
+
+    directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(0, 0, 5);
+    directionalLight.target.position.set(0, 0, 0);
+    directionalLight.intensity = 0.5; 
+    directionalLight.castShadow = true;
+    directionalLight.shadowDarkness = 0.5;
+     
+    directionalLight.shadowCameraNear = 0;
+    directionalLight.shadowCameraFar = 15;
+     
+    directionalLight.shadowCameraLeft = -5;
+    directionalLight.shadowCameraRight = 5;
+    directionalLight.shadowCameraTop = 5;
+    directionalLight.shadowCameraBottom = -5;
+     
+    scene.add(directionalLight);
+
+    var sprite = new SpriteText2D("Zoom back in", { align: textAlign.center,  font: '140px Arial', fillStyle: '#00ff00' , antialias: false })
+    sprite.position.z = 60;
+
+    scene.add(sprite)
+
     
     new THREE.OBJLoader()
         .setPath( './assets/webgl/geo/' )
@@ -179,19 +214,35 @@ function init() {
             material.side = THREE.DoubleSide;
 
             group.traverse( function ( child ) {
-
                 if ( child instanceof THREE.Mesh ) {
-
                     child.material = material;
-
                 }
-
-            } );
+            });
 
             group.position.x = 0 ;
             group.rotation.y = 0 ; - Math.PI / 2;
             group.rotation.x = Math.PI / 2; 
+
+            group.castShadow = true;
+
+            var geometry2 = new THREE.SphereGeometry(50, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+            var material2 = new THREE.MeshPhongMaterial( {color: 0x444444, side: THREE.BackSide});
+            var sphere = new THREE.Mesh(geometry2, material2);
+            //sphere.receiveShadow = true
+            sphere.castShadow = true
+
+            var geo = new THREE.PlaneBufferGeometry(2, 2, 1, 1);
+            var mat = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide });
+            var shadowPlane = new THREE.Mesh(geo, mat);
+            shadowPlane.position.x = 4.0;
+            shadowPlane.castShadow = true;
+            //shadowPlane.receiveShadow = true;
+
+
+            scene.add(sphere);
             scene.add( group );
+            //scene.add(shadowPlane);
+
 
         } );
 
@@ -253,7 +304,7 @@ function onKeyDown( e ) {
 
         controls.update();
         renderer.render( scene, camera );
-                renderer.setClearColor( 0xB4B4B4, 1 );
+                renderer.setClearColor( 0x000000, 1 );
         pointLight.position.x = camera.position.x;
         pointLight.position.y = camera.position.y;
         pointLight.position.z = camera.position.z;
@@ -261,6 +312,21 @@ function onKeyDown( e ) {
         pointLight.rotation.x = camera.rotation.x;
         pointLight.rotation.y = camera.rotation.y;
         pointLight.rotation.z = camera.rotation.z;
+
+        directionalLight.position.x = camera.position.x;
+        directionalLight.position.y = camera.position.y;
+        directionalLight.position.z = camera.position.z;
+
+        directionalLight.rotation.x = camera.rotation.x;
+        directionalLight.rotation.y = camera.rotation.y;
+        directionalLight.rotation.z = camera.rotation.z;
+
+        sprite.position.y = camera.position.y;
+        sprite.position.z = camera.position.z;
+
+        sprite.rotation.x = camera.rotation.x;
+        sprite.rotation.y = camera.rotation.y;
+        sprite.rotation.z = camera.rotation.z;
 
         if ( statsEnabled ) stats.update();
 
