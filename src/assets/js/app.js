95d7fafdd59cc12d88bb3d27615774e6495da5a7
Miles Thompson <utunga@gmail.com>
2018-09-21 13:55:11 +1200

    comment out things, mobile fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,26 +1,31 @@
 $('.select-source').slick({
-  centerMode: false,
   slidesToShow: 3,
   centerMode: false,
   dots: true,
+  lazyLoad: 'ondemand',
   responsive: [
     {
       breakpoint: 768,
       settings: {
         arrows: true,
-        centerMode: true,
-        slidesToShow: 3
+        slidesToShow: 1,
+        dots: false
       }
     },
     {
       breakpoint: 480,
       settings: {
         arrows: true,
-        centerMode: true,
-        slidesToShow: 3
+        slidesToShow: 1,
+        dots: false
       }
     }
   ],
 	prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
 	nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
 });
+
+
+// $(".select-source").on("afterChange", function (){
+//     $(".select-source .slick-active a").click();
+// })
